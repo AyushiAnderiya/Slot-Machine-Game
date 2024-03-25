a24e38dbf29df44e87e00099320499ecfd084ddb
AyushiAnderiya <ayushianderiya14@gmail.com>
2024-03-25 17:33:05 +0530

    designing the first phase

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,56 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const SlowM=()=>{
+  let x= '☃️';
+  let y= '☃️';
+  let z= '☀️';
 
+  if((x===y) && (x===z)){
+    return(
+      <>
+      <div className="game">
+        <h1>{x} {y} {z}</h1>
+        <h2>Hurreyyy Matching 😃</h2>
+        <hr />
+      </div>
+      </>
+    )
+  }
+  else{
+    return(
+      <>
+      <div className="game">
+        <h1>{x} {y} {z}</h1>
+        <h2>Try Again, Not Matching 🤕</h2>
+        <hr />
+      </div>
+      </>
+    )
+  }
+}
+
+function App() {
+  
   return (
     <>
-      
+      <div className="main">
+        <div className="head">
+        <h1>🎰 FortuneSpin: Spin & Win🎰</h1>  
+        </div>
+        <br /> <br />
+        <button className='btn'id='first' onClick={SlowM()}>1st Try</button>
+        
+        <button className='btn' id='second'  onClick={SlowM}>2nd Try</button>
+        
+        <button className='btn' id='third'  onClick={SlowM}>3rd Try</button>
+        
+        
+        
+      </div>
     </>
   )
 }
 
 export default App
+
